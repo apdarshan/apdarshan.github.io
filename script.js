5811ac70639259340de5397b7f23abc52b79a59b
Darshan <darpraka@cisco.com>
2014-08-17 12:38:12 +0530

    resume

---
+++ script.js
@@ -0,0 +1,87 @@
+var elements = document.getElementsByTagName("h1");
+console.log("AFTER KANNADA", elements.length);
+
+for(var i = 0; i < 500; i++) {
+	console.log("I:", i);
+}
+i = 0;
+for(var i = 0; i < 500; i++) {
+	console.log("I:", i);
+}
+i = 0;
+for(var i = 0; i < 500; i++) {
+	console.log("I:", i);
+}
+i = 0;
+for(var i = 0; i < 500; i++) {
+	console.log("I:", i);
+}
+i = 0;
+for(var i = 0; i < 500; i++) {
+	console.log("I:", i);
+}
+i = 0;
+for(var i = 0; i < 500; i++) {
+	console.log("I:", i);
+}
+for(var i = 0; i < 500; i++) {
+	console.log("I:", i);
+}
+i = 0;
+for(var i = 0; i < 500; i++) {
+	console.log("I:", i);
+}
+i = 0;
+for(var i = 0; i < 500; i++) {
+	console.log("I:", i);
+}
+i = 0;
+for(var i = 0; i < 500; i++) {
+	console.log("I:", i);
+}
+for(var i = 0; i < 500; i++) {
+	console.log("I:", i);
+}
+i = 0;
+for(var i = 0; i < 500; i++) {
+	console.log("I:", i);
+}
+i = 0;
+for(var i = 0; i < 500; i++) {
+	console.log("I:", i);
+}
+i = 0;
+for(var i = 0; i < 500; i++) {
+	console.log("I:", i);
+}
+for(var i = 0; i < 500; i++) {
+	console.log("I:", i);
+}
+i = 0;
+for(var i = 0; i < 500; i++) {
+	console.log("I:", i);
+}
+i = 0;
+for(var i = 0; i < 500; i++) {
+	console.log("I:", i);
+}
+i = 0;
+for(var i = 0; i < 500; i++) {
+	console.log("I:", i);
+}
+i = 0;
+for(var i = 0; i < 500; i++) {
+	console.log("I:", i);
+}
+i = 0;
+for(var i = 0; i < 500; i++) {
+	console.log("I:", i);
+}
+i = 0;
+for(var i = 0; i < 500; i++) {
+	console.log("I:", i);
+}
+i = 0;
+for(var i = 0; i < 500; i++) {
+	console.log("I:", i);
+}
